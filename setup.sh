1fed02694b2bc25f4e1e7b3ef8fab5d6059786f3
udaylunawat <udaylunawat@gmail.com>
2022-05-27 04:25:59 +0530

    Dataset Download

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/.kaggle
+cp kaggle.json ~/.kaggle
+
+mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume
+kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
+unzip data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
