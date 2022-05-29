ce7db11fa63bf919206d3e6c357d163939308524
udaylunawat <udaylunawat@gmail.com>
2022-05-29 18:28:24 +0530

    chmod

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ cp kaggle.json ~/.kaggle
 mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume
 kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
 unzip data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
+chmod 600 /root/.kaggle/kaggle.json
