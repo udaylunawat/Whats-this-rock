47887e05b4231f4bb062832e172d0f40737b8e47
udaylunawat <udaylunawat@gmail.com>
2022-05-30 01:01:27 +0530

    I don't like MINERALS for now!

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume
 kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
 unzip data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
 chmod 600 /root/.kaggle/kaggle.json
+rm -rf data/1_extracted/Rock_Dataset/minerals
