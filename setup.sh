2136a223dbc94cd7816fb574e995b6b4d519dd81
udaylunawat <udaylunawat@gmail.com>
2022-05-30 10:14:10 +0530

    Update: zip extraction is now quiet

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 mkdir -p ~/.kaggle
 cp kaggle.json ~/.kaggle
 
+rm -rf data
+rm training_data.csv
 mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume
 kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
-unzip data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
+unzip -q data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
 chmod 600 /root/.kaggle/kaggle.json
-rm -rf data/1_extracted/Rock_Dataset/minerals
