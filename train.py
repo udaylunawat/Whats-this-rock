838fce19d5e72ca474d32d2c80904e452de21de2
udaylunawat <udaylunawat@gmail.com>
2022-05-31 16:17:00 +0530

    Massively Updated augmentations

--- train.py
+++ train.py
@@ -217,9 +217,62 @@ def get_compiled_model(config, model):
     return model
 
 
+# https://datamonje.com/image-data-augmentation/#cutout
+# https://colab.research.google.com/drive/1on9rvQdr0s8CfqYeZBTbgk0K5I_Dha-X#scrollTo=py9wqsM05kio
+def custom_augmentation(np_tensor):
+    import tensorflow as tf
+    def random_contrast(np_tensor):
+        return np.array(tf.image.random_contrast(np_tensor, 0.5, 2))
+
+    def random_hue(np_tensor):
+        return np.array(tf.image.random_hue(np_tensor, 0.5))
+
+    def random_saturation(np_tensor):
+        return np.array(tf.image.random_saturation(np_tensor, 0.2, 3))
+
+    def random_crop(np_tensor):
+        # cropped height between 70% to 130% of an original height
+        new_height = int(np.random.uniform(0.7, 1.30) * np_tensor.shape[0])
+        # cropped width between 70% to 130% of an original width
+        new_width = int(np.random.uniform(0.7, 1.30) * np_tensor.shape[1])
+        # resize to new height and width
+        cropped = tf.image.resize_with_crop_or_pad(np_tensor, new_height, new_width)
+        return np.array(tf.image.resize(cropped, np_tensor.shape[:2]))
+
+    def gaussian_noise(np_tensor):
+        mean = 0
+        # variance: randomly between 1 to 25
+        var = np.random.randint(1, 26)
+        # sigma is square root of the variance value
+        noise = np.random.normal(mean, var**0.5, np_tensor.shape)
+        return np.clip(np_tensor + noise, 0, 255).astype('int')
+
+    def cutout(np_tensor):
+        cutout_height = int(np.random.uniform(0.1, 0.2) * np_tensor.shape[0])
+        cutout_width = int(np.random.uniform(0.1, 0.2) * np_tensor.shape[1])
+        cutout_height_point = np.random.randint(np_tensor.shape[0]-cutout_height)
+        cutout_width_point = np.random.randint(np_tensor.shape[1]-cutout_width)
+        np_tensor[cutout_height_point:cutout_height_point+cutout_height, cutout_width_point:cutout_width_point+cutout_width, :] = 127
+        return np_tensor
+
+    if (np.random.uniform() < 0.1):
+        np_tensor = random_contrast(np_tensor)
+    if (np.random.uniform() < 0.1):
+        np_tensor = random_hue(np_tensor)
+    if (np.random.uniform() < 0.1):
+        np_tensor = random_saturation(np_tensor)
+    if (np.random.uniform() < 0.2):
+        np_tensor = random_crop(np_tensor)
+    if (np.random.uniform() < 0.2):
+        np_tensor = gaussian_noise(np_tensor)
+    if (np.random.uniform() < 0.3):
+        np_tensor = cutout(np_tensor)
+    return np.array(np_tensor)
+
+
 def get_generators(config, train_data, test_data):
-    if config.augmentation == True:
-        datagen = ImageDataGenerator(horizontal_flip=False,
+    if config.augmentation is True:
+        datagen = ImageDataGenerator(horizontal_flip=True,
                                      featurewise_center=False,
                                      featurewise_std_normalization=False,
                                      zca_whitening=config.zca_whitening,
@@ -231,7 +284,7 @@ def get_generators(config, train_data, test_data):
                                      height_shift_range=config.height_shift_range,
                                      rotation_range=config.rotation_range,
                                      rescale=1. / 255.)
-    elif config.augmentation == False:
+    elif config.augmentation is False:
         datagen = ImageDataGenerator(rescale=1. / 255.)
 
     train_generator = datagen.flow_from_dataframe(
@@ -245,6 +298,7 @@ def get_generators(config, train_data, test_data):
         color_mode='rgb',
         shuffle=True,
         class_mode="categorical",
+        preprocessing_function = custom_augmentation,
         target_size=(
             config.size,
             config.size))
@@ -369,6 +423,7 @@ MODEL_NOTES = f'''
 Minerals Removed
 '''
 
+
 def SMOTE_Data(train_df):
     from imblearn.over_sampling import SMOTE
     from tensorflow.keras.preprocessing.image import load_img, img_to_array
@@ -443,19 +498,18 @@ if __name__ == "__main__":
     model.summary()
 
     class_weights = class_weight.compute_class_weight(
-                class_weight = 'balanced',
-                classes = np.unique(train_generator.classes),
-                y = train_generator.classes)
+                class_weight='balanced',
+                classes=np.unique(train_generator.classes),
+                y=train_generator.classes)
 
     train_class_weights = dict(enumerate(class_weights))
 
     callbacks = [WandbCallback(training_data=train_generator, validation_data=val_generator, input_type="image", labels=labels),
                  # ModelCheckpoint("save_at_{epoch}_ft_0_001.h5", save_best_only=True),
                  EarlyStopping(
-        monitor="val_f1_score",
-        min_delta=0.005,
-        patience=10,
-        mode='max')
+        monitor="val_loss",
+        min_delta=0.01,
+        patience=10)
     ]
     history = model.fit(
         train_generator,
