867b841ca526e28a3df89a74cb8995703f229e74
udaylunawat <udaylunawat@gmail.com>
2022-06-01 07:59:11 +0530

    refactor

---
+++ data_utilities.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+# https://towardsdatascience.com/stratified-sampling-you-may-have-been-splitting-your-dataset-all-wrong-8cfdd0d32502
+def get_stratified_dataset_partitions_pd(
+        df,
+        train_split=0.8,
+        val_split=0.1,
+        test_split=0.1,
+        target_variable=None):
+    assert (train_split + test_split + val_split) == 1
+
+    # Only allows for equal validation and test splits
+    assert val_split == test_split
+
+    # Shuffle
+    df_sample = df.sample(frac=1, random_state=12)
+
+    # Specify seed to always have the same split distribution between runs
+    # If target variable is provided, generate stratified sets
+    if target_variable is not None:
+        grouped_df = df_sample.groupby(target_variable)
+        arr_list = [np.split(g,
+                             [int(train_split * len(g)),
+                              int((1 - val_split) * len(g))]) for i,
+                    g in grouped_df]
+
+        train_ds = pd.concat([t[0] for t in arr_list])
+        val_ds = pd.concat([t[1] for t in arr_list])
+        test_ds = pd.concat([v[2] for v in arr_list])
+
+    else:
+        indices_or_sections = [
+            int(train_split * len(df)), int((1 - val_split) * len(df))]
+        train_ds, val_ds, test_ds = np.split(df_sample, indices_or_sections)
+
+    return train_ds, val_ds, test_ds
+
+
+def get_data(sample_size):
+    data = pd.read_csv('training_data.csv', index_col=0)
+    data = data.sample(frac=sample_size).reset_index(drop=True)
+    # Splitting data into train, val and test samples using stratified splits
+    train_df, val_df, test_df = get_stratified_dataset_partitions_pd(
+        data, 0.8, 0.1, 0.1)
+    train_df = pd.concat([train_df, val_df])
+    return train_df, test_df
+
+
+def undersample_df(data, class_name):
+    merged_df = pd.DataFrame()
+    for rock_type in data[class_name].unique():
+        temp = data[data[class_name] == rock_type].sample(n=min(data[class_name].value_counts()))
+        merged_df = pd.concat([merged_df, temp])
+
+    return merged_df
+
+
+def get_all_filePaths(folderPath):
+    result = []
+    for dirpath, dirnames, filenames in os.walk(folderPath):
+        result.extend([os.path.join(dirpath, filename) for filename in filenames if filename[-3:] == 'jpg'])
+    return result
+
+
+# make some random data
+# reset_random_seeds()
+
+# default config/hyperparameter values
+# you can modify these below or via command line
+# https://github.com/wandb/examples/blob/master/examples/keras/keras-cnn-fashion/cnn_train.py
+
+
+def SMOTE_Data(config, train_df):
+    from imblearn.over_sampling import SMOTE
+    from tensorflow.keras.preprocessing.image import load_img, img_to_array
+    X_train = []
+    for img in train_df['image_path']:
+        loaded_img = load_img(os.path.join(IMAGE_DIR, img), target_size=(config.image_size, config.image_size))
+        img_arr = img_to_array(loaded_img)
+        X_train.append(img_arr)
+
+    print(np.array(X_train).shape)
+    y_train = train_df.drop('image_path', axis=1, inplace=False)
+    print(y_train.head())
+    y_train = np.array(y_train.values)
+    X_train = np.array(X_train)
+    sm = SMOTE(random_state=42)
+    X_train, y_train = sm.fit_resample(X_train.reshape((-1, config.image_size * config.image_size * 3)), y_train)
+    X_train.reshape(-1, config.image_size, config.image_size, 3)
+    return X_train, y_train
