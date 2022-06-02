65cf63c8d5644de9ab9e56799b98df5e212e70d9
udaylunawat <udaylunawat@gmail.com>
2022-06-02 17:27:45 +0530

    removing corrupted images

--- data_utilities.py
+++ data_utilities.py
@@ -3,6 +3,73 @@ import numpy as np
 import os
 
 
+def remove_corrupted_images(root_dir):
+    import os
+    import cv2
+    def check_images( s_dir, ext_list):
+        bad_images=[]
+        bad_ext=[]
+        s_list= os.listdir(s_dir)
+        for klass in s_list:
+            klass_path=os.path.join (s_dir, klass)
+            print ('processing class directory ', klass)
+            if os.path.isdir(klass_path):
+                file_list=os.listdir(klass_path)
+                for f in file_list:
+                    f_path=os.path.join (klass_path,f)
+                    index=f.rfind('.')
+                    ext=f[index+1:].lower()
+                    if ext not in ext_list:
+                        print('file ', f_path, ' has an invalid extension ', ext)
+                        bad_ext.append(f_path)
+                    if os.path.isfile(f_path):
+                        try:
+                            img=cv2.imread(f_path)
+                            shape=img.shape
+                        except:
+                            print('file ', f_path, ' is not a valid image file')
+                            bad_images.append(f_path)
+                    else:
+                        print('*** fatal error, you a sub directory ', f, ' in class directory ', klass)
+            else:
+                print ('*** WARNING*** you have files in ', s_dir, ' it should only contain sub directories')
+        return bad_images, bad_ext
+
+    source_dir = root_dir
+    good_exts=['jpg', 'png', 'jpeg', 'gif', 'bmp' ] # list of acceptable extensions
+    bad_file_list, bad_ext_list=check_images(source_dir, good_exts)
+    if len(bad_file_list) !=0:
+        print('improper image files are listed below')
+        for i in range (len(bad_file_list)):
+            print (bad_file_list[i])
+    else:
+        print(' no improper image files were found')
+
+    for bad_file in bad_file_list:
+        os.remove(bad_file)
+
+
+
+    from pathlib import Path
+    import imghdr
+
+    # https://stackoverflow.com/a/68192520/9292995
+    for class_name in os.listdir(root_dir):
+        data_dir = os.path.join(root_dir, class_name)
+        image_extensions = [".png", ".jpg"]  # add there all your images file extensions
+
+        img_type_accepted_by_tf = ["bmp", "gif", "jpeg", "png"]
+        for filepath in Path(data_dir).rglob("*"):
+            if filepath.suffix.lower() in image_extensions:
+                img_type = imghdr.what(filepath)
+                if img_type is None:
+                    print(f"{filepath} is not an image")
+                    os.remove(filepath)
+                elif img_type not in img_type_accepted_by_tf:
+                    print(f"{filepath} is a {img_type}, not accepted by TensorFlow")
+                    os.remove(filepath)
+
+
 # https://towardsdatascience.com/stratified-sampling-you-may-have-been-splitting-your-dataset-all-wrong-8cfdd0d32502
 def get_stratified_dataset_partitions_pd(
         df,
