7a8b4328dd985eb5b7f0e956724ca65c8b5ced40
udaylunawat <udaylunawat@gmail.com>
2022-06-02 12:27:52 +0530

    csv no longer in root

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ mkdir -p ~/.kaggle
 cp kaggle.json ~/.kaggle
 
 rm -rf data
-rm training_data.csv
 mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume
 kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
 unzip -q data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
