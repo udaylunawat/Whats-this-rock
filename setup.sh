76f1f780c24ff7d6d9757801ac97cc3bd5bd5146
udaylunawat <udaylunawat@gmail.com>
2022-07-16 13:09:58 +0530

    update: 4_tfds_dataset directory

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.kaggle
 cp kaggle.json ~/.kaggle
 
 rm -rf data
-mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume
+mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset
 kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
 unzip -q data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
 chmod 600 /root/.kaggle/kaggle.json
