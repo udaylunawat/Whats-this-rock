dd400e3a2a35ffd53d039ac44355ba312e162add
udaylunawat <udaylunawat@gmail.com>
2022-07-17 05:28:59 +0530

    bug fix: directory error

--- preprocess.py
+++ preprocess.py
@@ -14,21 +14,24 @@ def setup_dirs_and_preprocess(args):
     all_paths = []
     all_classes = []
 
-    class_dirs = os.listdir(args.root)
-    for class_name in class_dirs:
-        os.makedirs(os.path.join('data/2_processed', class_name), exist_ok=True)
-        paths_list = get_all_filePaths(os.path.join(args.root, class_name))
+    for dataset in os.listdir(args.root):
+        class_dirs = os.listdir(os.path.join(args.root ,dataset))
+        for class_name in class_dirs:
+            os.makedirs(os.path.join('data/2_processed', class_name), exist_ok=True)
 
-        for image_path in paths_list:
-            source = image_path
-            target_file_name = '-'.join([image_path.split('/')[i] for i in range(-3, 0)])
-            target = os.path.join('data/2_processed', class_name, target_file_name)
-            shutil.move(source, target)
-            if os.path.isfile(source):
-                os.remove(source)
+            paths_list = get_all_filePaths(os.path.join(args.root, dataset, class_name))
 
-            all_paths.append(os.path.join(target, os.path.basename(source)))
-            all_classes.append(class_name)
+            for image_path in paths_list:
+                source = image_path
+                target_file_name = '-'.join([image_path.split('/')[i] for i in range(-3, 0)])
+                target = os.path.join('data/2_processed', class_name, target_file_name)
+                # print(f"Moving from {source} to {target}")
+                shutil.move(source, target)
+                if os.path.isfile(source):
+                    os.remove(source)
+
+                all_paths.append(os.path.join(target))
+                all_classes.append(class_name)
 
     shutil.rmtree(args.root)
     data = pd.DataFrame(list(zip(all_paths, all_classes)),
@@ -66,18 +69,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     root_dir = 'data/1_extracted/'
+
     remove_corrupted_images(root_dir)
     data = setup_dirs_and_preprocess(args)
+    print("Splitting files in Train, Validation and Test and saving to data/4_tfds_dataset/")
     if args.oversample:
         # If your datasets is balanced (each class has the same number of samples), choose ratio otherwise fixed.
         splitfolders.fixed('data/2_processed', output="data/4_tfds_dataset", oversample=True,
                            seed=1337)
     elif args.undersample:
         splitfolders.fixed('data/2_processed', output="data/4_tfds_dataset",
-                           fixed=(int(args.undersample * 0.8), int(args.undersample * 0.2), int(args.undersample * 0.2)),
+                           fixed=(int(args.undersample * 0.75), int(args.undersample * 0.125), int(args.undersample * 0.125)),
                            oversample=False,
                            seed=1337)
     else:
         splitfolders.ratio('data/2_processed', output="data/4_tfds_dataset",
-                           ratio=(0.75, 0.25, 0.25),
+                           ratio=(0.75, 0.125, 0.125),
                            seed=1337)
