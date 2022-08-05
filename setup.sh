e73a18144f9b2f469bd17f7d0f84d8c4cad5cdea
udaylunawat <udaylunawat@gmail.com>
2022-08-06 01:16:59 +0530

    ref: corrupted_images in data

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ mkdir -p ~/.kaggle
 cp kaggle.json ~/.kaggle
 chmod 600 /root/.kaggle/kaggle.json
 
-rm -rf data corrupted_images
-mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset
+rm -rf data
+mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images
 # kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
 kaggle datasets download salmaneunus/rock-classification --path data/0_raw/
 # unzip -q data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
