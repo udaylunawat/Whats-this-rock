61613871bab0b7fbce592ed7c82729974e8d3195
udaylunawat <udaylunawat@gmail.com>
2022-08-13 22:15:28 +0530

    ref

--- augment_utilities.py
+++ augment_utilities.py
@@ -1,18 +1,6 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import keras_cv
-
-
-rand_augment = keras_cv.layers.RandAugment(
-    value_range=(0, 255),
-    augmentations_per_image=3,
-    magnitude=0.3,
-    magnitude_stddev=0.2,
-    rate=0.5,
-)
-
-cut_mix = keras_cv.layers.CutMix()
-mix_up = keras_cv.layers.MixUp()
+# import keras_cv
 
 
 def visualize_dataset(dataset, title):
@@ -26,11 +14,21 @@ def visualize_dataset(dataset, title):
 
 
 def apply_rand_augment(inputs):
+
+    rand_augment = keras_cv.layers.RandAugment(
+        value_range=(0, 255),
+        augmentations_per_image=3,
+        magnitude=0.3,
+        magnitude_stddev=0.2,
+        rate=0.5,
+    )
     inputs["images"] = rand_augment(inputs["images"])
     return inputs
 
 
 def cut_mix_and_mix_up(samples):
+    cut_mix = keras_cv.layers.CutMix()
+    mix_up = keras_cv.layers.MixUp()
     samples = cut_mix(samples, training=True)
     samples = mix_up(samples, training=True)
     return samples
