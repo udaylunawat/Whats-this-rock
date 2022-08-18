8a9726d23228ae613486de1b3232d05b913c79f2
udaylunawat <udaylunawat@gmail.com>
2022-08-18 09:39:22 +0530

    ref: removed wandb

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,7 +5,7 @@ from data_utilities import get_generators
 import os
 import plot
 import json
-import wandb
+# import wandb
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,22 +16,24 @@ from sklearn.metrics import classification_report, confusion_matrix, ConfusionMa
 with open('config.json') as config_file:
     config = json.load(config_file)
 
+# run = wandb.init(project="Whats-this-rock-inceptionresnetv2",
+#                  entity="rock-classifiers",
+#                  config=config)
+
 # os.system('wget -O model-best.h5 https://www.dropbox.com/s/x91k9u765urnlai/model-best.h5')
-run = wandb.init(project="Whats-this-rock",
-                 entity="rock-classifiers",
-                 config=config)
-
-if not os.path.exists('model-best.h5'):
-
-    api = wandb.Api()
-    run = api.run("rock-classifiers/Whats-this-rock/3krh6juu")
-    run.file("model-best.h5").download()
+# if not os.path.exists('model-best.h5'):
+#     api = wandb.Api()
+#     run = api.run("rock-classifiers/Whats-this-rock/3hvgnqas")
+#     run.file("model-best.h5").download()
 
 train_dataset, val_dataset, test_dataset = get_generators(config)
 labels = ['Basalt', 'Coal', 'Granite', 'Limestone', 'Marble', 'Quartzite', 'Sandstone']
 
 model = get_model(config)
-model.load_weights('model-best.h5')
+try:
+    model.load_weights('model-best.h5')
+except:
+    print("model-best.h5 should be present in the checkpoint dir.")
 
 opt = get_optimizer(config)
 
@@ -46,9 +48,9 @@ model.compile(loss=config['loss_fn'],
 
 # Scores
 scores = model.evaluate(test_dataset, return_dict=True)
-print('Accuracy: ', scores)
-wandb.log({'Test Accuracy':scores['accuracy']})
-wandb.log({'Test F1 Score':scores['f1_score']})
+print('Scores: ', scores)
+# wandb.log({'Test Accuracy': scores['accuracy']})
+# wandb.log({'Test F1 Score': scores['f1_score']})
 
 # Predict
 pred = model.predict(test_dataset, verbose=1)
@@ -56,28 +58,16 @@ predicted_class_indices = np.argmax(pred, axis=1)
 
 # Confusion Matrix
 cm = plot.confusion_matrix(labels, test_dataset.classes, predicted_class_indices)
-wandb.log({"Confusion Matrix":cm})
-
-cm = wandb.plot.confusion_matrix(
-    y_true=test_dataset.classes,
-    preds=predicted_class_indices,
-    class_names=labels)
-wandb.log({"conf_mat": cm})
+# wandb.log({"Confusion Matrix": cm})
 
 # Classification Report
 cl_report = classification_report(test_dataset.classes,
-                                   predicted_class_indices,
-                                   labels=[0,1,2,3,4,5,6],
-                                   target_names=labels,
-                                   output_dict=True)
-
-df = pd.DataFrame(cl_report)
-wandb.Table(dataframe=df)
-wandb.log({"Classification Report: cl_report"})
-
-cl_bar_plot = df.iloc[:3, :3].T.plot(kind='bar')
-wandb.log({"CR Bar Plot": wandb.Plotly(cl_bar_plot)})
+                                  predicted_class_indices,
+                                  labels=[0, 1, 2, 3, 4, 5, 6],
+                                  target_names=labels,
+                                  output_dict=True)
+print(cl_report)
 
 cr = sns.heatmap(pd.DataFrame(cl_report).iloc[:-1, :].T, annot=True)
-wandb.log({"Classification Report": wandb.Plotly(cr)})
-plt.savefig('cr.png', dpi=400)
+# plt.savefig('imgs/cr.png', dpi=400)
+# wandb.log({"Classification Report Image:":wandb.Image('imgs/cr.png', caption="Classification Report")})
