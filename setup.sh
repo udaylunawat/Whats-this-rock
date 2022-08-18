c905f869432e5ef33317a34038742740a3e84b88
udaylunawat <udaylunawat@gmail.com>
2022-08-18 11:08:23 +0530

    feat: use any dataset

--- setup.sh
+++ setup.sh
@@ -11,6 +11,12 @@ pip install -r requirements-dev.txt
 # setting up data dir
 rm -rf data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images
 mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images checkpoints
+mkdir -p data/2_processed/Basalt/
+mkdir -p data/2_processed/Granite/
+mkdir -p data/2_processed/Marble/
+mkdir -p data/2_processed/Quartzite/
+mkdir -p data/2_processed/Limestone/
+mkdir -p data/2_processed/Sandstone/
 
 # dataset 1 processing
 kaggle datasets download salmaneunus/rock-classification --path data/0_raw/
