2b030ae812fa3ddf1bc36039c67a4fc72b499ca1
udaylunawat <udaylunawat@gmail.com>
2022-08-19 05:15:46 +0530

    ref

--- src/train.py
+++ src/train.py
@@ -58,84 +58,71 @@ class custom_callback(Callback):
                 os.remove(os.path.join('checkpoints', file_name))
 
 
-# read config file
-with open('config.json') as config_file:
-    default = json.load(config_file)
+def train(config):
+    with wandb.init() as run:
 
+        # model = load_model('checkpoints/visionary-sweep-10-efficientnet-epoch-2-val_f1_score-0.65.hdf5')
 
-if __name__ == "__main__":
+        file_name = "model-best.h5"
+        if config['finetune']:
+            os.remove('model-best.h5')
+            api = wandb.Api()
+            run = api.run("rock-classifiers/Whats-this-rockv2/cvzc7hq0")  # different-sweep-34-efficientnet-epoch-3-val_f1_score-0.71.hdf5
+            run.file(file_name).download()
+            model = load_model(file_name)
+            print("Downloaded Trained model, finetuning...")
+        else:
+            # build model
+            K.clear_session()
+            model = get_model(config)
 
-    reset_random_seeds()
-    print(f"Default config:- {json.dumps(default, indent=2)}\n P.S - Not used in sweeps.\n\n")
-    run = wandb.init(project="Whats-this-rockv2",
-                     entity="rock-classifiers",
-                     config=default, allow_val_change=True)
-    config = wandb.config
-    IMAGE_SIZE = (config["image_size"], config["image_size"])
+        # print(model.summary())
 
-    train_dataset, val_dataset, test_dataset = get_generators(config)
-    labels = ['Basalt', 'Coal', 'Granite', 'Limestone', 'Marble', 'Quartzite', 'Sandstone']
-    config['num_classes'] = len(labels)
-    wandb.log({'num_classes':len(labels)})
+        print(f"Model loaded: {model.name}\n\n")
+        opt = get_optimizer(config)
 
-    class_weights = get_model_weights(train_dataset)
+        config['metrics'].append(tfa.metrics.F1Score(
+            num_classes=config['num_classes'],
+            average='macro',
+            threshold=0.5))
 
-    # model = load_model('checkpoints/visionary-sweep-10-efficientnet-epoch-2-val_f1_score-0.65.hdf5')
+        class_weights = get_model_weights(train_dataset)
 
-    file_name = "model-best.h5"
-    if config['finetune']:
-        os.remove('model-best.h5')
-        api = wandb.Api()
-        run = api.run("rock-classifiers/Whats-this-rockv2/cvzc7hq0") # different-sweep-34-efficientnet-epoch-3-val_f1_score-0.71.hdf5
-        run.file(file_name).download()
-        model = load_model(file_name)
-        print("Downloaded Trained model, finetuning...")
-    else:
-        # build model
-        K.clear_session()
-        model = get_model(config)
-
-    # print(model.summary())
-
-    print(f"Model loaded: {model.name}\n\n")
-    opt = get_optimizer(config)
-
-    config['metrics'].append(tfa.metrics.F1Score(
-        num_classes=config['num_classes'],
-        average='macro',
-        threshold=0.5))
-
-    model.compile(loss=config['loss_fn'],
-                  optimizer=opt,
-                  metrics=config["metrics"])
-    model_checkpoint = ModelCheckpoint("checkpoints/"+f"{wandb.run.name}-" + config["model_name"]+
-                                       "-epoch-{epoch}-val_f1_score-{val_f1_score:.2f}.hdf5", save_best_only=True)
-    reduce_lr = ReduceLROnPlateau(monitor="val_f1_score", factor=config['lr_reduce_factor'], patience=config['lr_reduce_patience'], verbose=1, min_lr=0.0000001)
-    earlystopper = EarlyStopping(
-        monitor='val_f1_score', patience=config['earlystopping_patience'], verbose=1, mode='auto', min_delta=config['earlystopping_min_delta'],
-        restore_best_weights=True
-    )
-    # Define WandbCallback for experiment tracking
-    wandbcallback = WandbCallback(monitor="val_f1_score",
-                                  save_model=(True),
-                                  save_graph=(False)
-                                  )
-    # callbacks = [wandbcallback, earlystopper, model_checkpoint, reduce_lr, custom_callback()]
-    callbacks = [LRA(wandb=wandb, model=model, patience=config['lr_reduce_patience'], stop_patience=config['earlystopping_patience'], threshold=.75,
-                     factor=config['lr_reduce_factor'], dwell=False, model_name=config['model_name'], freeze=config['freeze'], initial_epoch=0),
+        model.compile(loss=config['loss_fn'],
+                      optimizer=opt,
+                      metrics=config["metrics"])
+        model_checkpoint = ModelCheckpoint("checkpoints/"+f"{wandb.run.name}-" + config["model_name"]+
+                                           "-epoch-{epoch}-val_f1_score-{val_f1_score:.2f}.hdf5", save_best_only=True)
+        reduce_lr = ReduceLROnPlateau(monitor="val_f1_score", factor=config['lr_reduce_factor'], patience=config['lr_reduce_patience'], verbose=1, min_lr=0.0000001)
+        earlystopper = EarlyStopping(
+            monitor='val_f1_score', patience=config['earlystopping_patience'], verbose=1, mode='auto', min_delta=config['earlystopping_min_delta'],
+            restore_best_weights=True
+        )
+        # Define WandbCallback for experiment tracking
+        wandbcallback = WandbCallback(monitor="val_f1_score",
+                                      save_model=(True),
+                                      save_graph=(False)
+                                      )
+        # callbacks = [wandbcallback, earlystopper, model_checkpoint, reduce_lr, custom_callback()]
+        callbacks = [LRA(wandb=wandb, model=model, patience=config['lr_reduce_patience'], stop_patience=config['earlystopping_patience'], threshold=.80,
+                         factor=config['lr_reduce_factor'], dwell=False, model_name=config['model_name'], freeze=config['freeze'], initial_epoch=0),
                      model_checkpoint, wandbcallback, custom_callback()]
-    LRA.tepochs = config['max_epochs']  # used to determine value of last epoch for printing
+        LRA.tepochs = config['max_epochs']  # used to determine value of last epoch for printing
 
-    history = model.fit(
-        train_dataset,
-        epochs=config["max_epochs"],
-        validation_data=val_dataset,
-        callbacks=callbacks,
-        class_weight=class_weights,
-        workers=-1,
-        verbose=0,
-    )
+        history = model.fit(
+            train_dataset,
+            epochs=config["max_epochs"],
+            validation_data=val_dataset,
+            callbacks=callbacks,
+            class_weight=class_weights,
+            workers=-1,
+            verbose=0,
+        )
 
+        return model
+
+
+def evaluate(config, model, test_dataset):
     # Scores
     scores = model.evaluate(test_dataset, return_dict=True)
     print('Scores: ', scores)
@@ -161,5 +148,28 @@ if __name__ == "__main__":
     cr = sns.heatmap(pd.DataFrame(cl_report).iloc[:-1, :].T, annot=True)
     plt.savefig('imgs/cr.png', dpi=400)
     wandb.log({"Classification Report Image:": wandb.Image('imgs/cr.png', caption="Classification Report")})
+
+
+# read config file
+with open('config.json') as config_file:
+    default = json.load(config_file)
+
+
+if __name__ == "__main__":
+
+    reset_random_seeds()
+    print(f"Default config:- {json.dumps(default, indent=2)}\n P.S - Not used in sweeps.\n\n")
+    run = wandb.init(project="Whats-this-rockv2",
+                     entity="rock-classifiers",
+                     config=default, allow_val_change=True)
+    config = wandb.config
+    IMAGE_SIZE = (config["image_size"], config["image_size"])
+
+    train_dataset, val_dataset, test_dataset = get_generators(config)
+    labels = ['Basalt', 'Coal', 'Granite', 'Limestone', 'Marble', 'Quartzite', 'Sandstone']
+    config['num_classes'] = len(labels)
+
+    model = train(config)
+    evaluate(config, model)
 
     run.finish()
