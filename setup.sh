3ed18b0cb83031421697887c4b921120892d7d20
udaylunawat <udaylunawat@gmail.com>
2022-08-19 05:20:11 +0530

    ref

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# installing
+apt install --allow-change-held-packages libcudnn8=8.1.0.77-1+cuda11.2
+pip install -r requirements-dev.txt
+
 # setting up kaggle
 wget https://www.dropbox.com/s/9mhxs1p4mgb8gyh/kaggle.json
 mkdir -p ~/.kaggle
 mv kaggle.json ~/.kaggle
 chmod 600 /root/.kaggle/kaggle.json
-
-pip install -r requirements-dev.txt
 
 # setting up data dir
 rm -rf data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images
