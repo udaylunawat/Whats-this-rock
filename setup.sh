0896b5949a53161d356fe55b8317eaeaf7c283ba
udaylunawat <udaylunawat@gmail.com>
2022-08-22 23:38:41 +0530

    fix: kaggle.json

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt install --allow-change-held-packages libcudnn8=8.1.0.77-1+cuda11.2
 pip install -r requirements-dev.txt
 
 # setting up kaggle
-wget https://www.dropbox.com/s/9mhxs1p4mgb8gyh/kaggle.json
+wget https://www.dropbox.com/s/ltp4ly8ilvxlgas/kaggle.json
 mkdir -p ~/.kaggle
 mv kaggle.json ~/.kaggle
 chmod 600 /root/.kaggle/kaggle.json
