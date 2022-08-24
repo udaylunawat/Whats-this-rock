483ea6d324df09b25c9254bc5c99ccf90fa6839f
udaylunawat <udaylunawat@gmail.com>
2022-08-24 13:16:08 +0530

    fix: threshold 0.9

--- src/train.py
+++ src/train.py
@@ -92,7 +92,7 @@ def train():
                     metrics=config["metrics"])
     model_checkpoint = ModelCheckpoint("checkpoints/"+f"{wandb.run.name}-" + config["model_name"]+
                                         "-epoch-{epoch}-val_f1_score-{val_f1_score:.2f}.hdf5", save_best_only=True)
-    reduce_lr = ReduceLROnPlateau(monitor="val_f1_score", factor=config['lr_reduce_factor'], patience=config['lr_reduce_patience'], verbose=1, min_lr=0.0000001)
+    reduce_lr = ReduceLROnPlateau(monitor="val_f1_score", factor=config['lr_reduce_factor'], patience=config['lr_reduce_patience'], verbose=1, min_lr=0.000001)
     earlystopper = EarlyStopping(
         monitor='val_f1_score', patience=config['earlystopping_patience'], verbose=1, mode='auto', min_delta=config['earlystopping_min_delta'],
         restore_best_weights=True
@@ -103,7 +103,7 @@ def train():
                                     save_graph=(False)
                                     )
     # callbacks = [wandbcallback, earlystopper, model_checkpoint, reduce_lr, custom_callback()]
-    callbacks = [LRA(wandb=wandb, model=model, patience=config['lr_reduce_patience'], stop_patience=config['earlystopping_patience'], threshold=.80,
+    callbacks = [LRA(wandb=wandb, model=model, patience=config['lr_reduce_patience'], stop_patience=config['earlystopping_patience'], threshold=.9,
                         factor=config['lr_reduce_factor'], dwell=False, model_name=config['model_name'], freeze=config['freeze'], initial_epoch=0),
                     model_checkpoint, wandbcallback, custom_callback()]
     LRA.tepochs = config['max_epochs']  # used to determine value of last epoch for printing
