88fa9b994452978e8251f2396401a4089360c5af
udaylunawat <udaylunawat@gmail.com>
2022-08-31 12:50:22 +0530

    fix: Coal

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ chmod 600 /root/.kaggle/kaggle.json
 # setting up data dir
 rm -rf data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images
 mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images checkpoints
+mkdir -p data/2_processed/Coal/
 mkdir -p data/2_processed/Basalt/
 mkdir -p data/2_processed/Granite/
 mkdir -p data/2_processed/Marble/
