fba77690c4b6cd73aa4d9f533cb45e06cb804161
udaylunawat <udaylunawat@gmail.com>
2022-09-01 14:28:17 +0530

    fix: wandb logs now saves preprocess steps

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,20 +1,10 @@
 import os
 import shutil
-import argparse
 
 # https://stackoverflow.com/a/64006242/9292995
 import splitfolders
 from data_utilities import remove_corrupted_images, get_df
 
-from absl import app
-from absl import flags
-from ml_collections.config_flags import config_flags
-from ml_collections import config_dict
-from ml_collections import config_flags
-
-# Config
-FLAGS = flags.FLAGS
-CONFIG = config_flags.DEFINE_config_file("config", "configs/baseline.py")
 
 def create_classes_dir(args):
     for dataset in os.listdir(args.root):
@@ -29,10 +19,10 @@ def create_classes_dir(args):
 
     shutil.rmtree(args.root)
 
-def main(_):
+def process_data(config):
 
     # Get configs from the config file.
-    args = CONFIG.value
+    args = config
 
     # create_classes_dir(args)
     # remove_corrupted_images('data/2_processed')
@@ -41,11 +31,11 @@ def main(_):
     print(
         "\nSplitting files in Train, Validation and Test and saving to data/4_tfds_dataset/"
     )
+    scc = min(get_df()["class"].value_counts())
     if args.dataset_config.sampling == 'oversample':
-        print("Oversampling...")
+        print("\nOversampling...")
         # If your datasets is balanced (each class has the same number of samples), choose ratio otherwise fixed.
         print("Finding smallest class for oversampling fixed parameter.")
-        scc = min(get_df()["class"].value_counts())
         print(f"Smallest class count is {scc}\n")
         splitfolders.fixed(
             "data/2_processed",
@@ -54,15 +44,15 @@ def main(_):
             fixed=(((scc // 2) - 1, (scc // 2) - 1)),
             seed=args.seed,
         )
-    elif isinstance(args.dataset_config.sampling, (int, float)):
+    elif args.dataset_config.sampling == 'undersample':
         print(f"Undersampling to {args.dataset_config.sampling} samples.")
         splitfolders.fixed(
             "data/2_processed",
             output="data/4_tfds_dataset",
             fixed=(
-                int(args.dataset_config.sampling * 0.75),
-                int(args.dataset_config.sampling * 0.125),
-                int(args.dataset_config.sampling * 0.125),
+                int(scc * 0.75),
+                int(scc * 0.125),
+                int(scc * 0.125),
             ),
             oversample=False,
             seed=args.seed,
@@ -75,5 +65,4 @@ def main(_):
             ratio=(0.75, 0.125, 0.125),
             seed=args.seed,
         )
-if __name__ == "__main__":
-    app.run(main)
+    print('\n\n')
