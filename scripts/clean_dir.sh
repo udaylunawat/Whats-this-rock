6a4b66493b3b5d264808ff6d9d1ff8e27a878a58
udaylunawat <udaylunawat@gmail.com>
2022-09-08 02:27:43 +0530

    fix: removing 1_extracted

--- scripts/clean_dir.sh
+++ scripts/clean_dir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setting up data dir
-rm -rf data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images
+rm -rf data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images
 mkdir -p data/0_raw data/1_extracted data/2_processed data/3_consume data/4_tfds_dataset data/corrupted_images checkpoints
 mkdir -p data/2_processed/Coal/
 mkdir -p data/2_processed/Basalt/
