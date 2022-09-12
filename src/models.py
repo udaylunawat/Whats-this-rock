843a4fe67e738aa70d480550a96d17445b2f67d8
udaylunawat <udaylunawat@gmail.com>
2022-09-13 03:20:40 +0530

    ref

--- src/models.py
+++ src/models.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import models
+from tensorflow.keras import layers, models, applications
 from tensorflow.keras import regularizers, initializers
 
 
@@ -11,36 +10,26 @@ def get_backbone(args):
         args (ml_collections.ConfigDict): Configuration.
     """
     weights = None
+    models_dict = {
+        'vgg16': applications.VGG16,
+        'resnet': applications.ResNet50,
+        'inceptionresnetv2': applications.InceptionResNetV2,
+        'mobilenetv2': applications.MobileNetV2,
+        'efficientnetv2': applications.EfficientNetV2B0,
+        'efficientnetv2m': applications.EfficientNetV2M
+    }
+
     if args.model_config.use_pretrained_weights:
         weights = "imagenet"
 
-    if args.model_config.backbone == 'vgg16':
-        base_model = tf.keras.applications.VGG16(include_top=False,
-                                                 weights=weights)
-
-    elif args.model_config.backbone == 'resnet':
-        base_model = tf.keras.applications.ResNet50(include_top=False,
-                                                    weights=weights)
-
-    elif args.model_config.backbone == 'inceptionresnetv2':
-        base_model = tf.keras.applications.InceptionResNetV2(include_top=False,
-                                                             weights=weights)
-
-    elif args.model_config.backbone == 'mobilenetv2':
-        base_model = tf.keras.applications.MobileNetV2(include_top=False,
-                                                       weights=weights)
-
-    elif args.model_config.backbone == 'efficientnet':
-        base_model = tf.keras.applications.EfficientNetV2B0(include_top=False,
-                                                            weights=weights)
-
-    elif args.model_config.backbone == 'EfficientNetV2M':
-        base_model = tf.keras.applications.EfficientNetV2M(include_top=False,
-                                                           weights=weights)
-
-    else:
+    try:
+        base_model = models_dict[args.model_config.backbone]
+    except:
         raise NotImplementedError("Not implemented for this backbone.")
+
+    base_model = base_model(include_top=False, weights=weights)
     base_model.trainable = args.model_config.trainable
+
     return base_model
 
 
@@ -54,18 +43,16 @@ def get_model(args):
     # Backbone
     base_model = get_backbone(args)
 
-    model = tf.keras.Sequential(
-        [
-            base_model,
-            layers.GlobalAveragePooling2D(),
-            layers.Dense(1024),
-            layers.Dropout(args.model_config.dropout_rate),
-            layers.Dense(256),
-            layers.Dropout(args.model_config.dropout_rate),
-            layers.Dense(64),
-            layers.Dropout(args.model_config.dropout_rate),
-            layers.Dense(args.dataset_config.num_classes, activation="softmax"),
-        ]
-    )
+    model = tf.keras.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(1024),
+        layers.Dropout(args.model_config.dropout_rate),
+        layers.Dense(256),
+        layers.Dropout(args.model_config.dropout_rate),
+        layers.Dense(64),
+        layers.Dropout(args.model_config.dropout_rate),
+        layers.Dense(args.dataset_config.num_classes, activation="softmax"),
+    ])
 
     return model
