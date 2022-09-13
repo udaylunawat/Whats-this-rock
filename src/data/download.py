2425665df1dfa977b9e725137acfa4120ea33e07
udaylunawat <udaylunawat@gmail.com>
2022-09-14 02:12:56 +0530

    feat: download/extracted dataset only if not already done

--- src/data/download.py
+++ src/data/download.py
@@ -1,10 +1,11 @@
 import os
 
-def get_data(script_id):
+def get_data(dataset_id):
     data_dict = {
         1:'src/scripts/dataset1.sh',
         2:'src/scripts/dataset2.sh',
         3:'src/scripts/dataset3.sh',
         4:'src/scripts/dataset4.sh'
     }
-    os.system(f"sh {data_dict[script_id]}")
+    if not os.path.exists(os.path.join('data', '1_extracted', f"dataset{dataset_id}")):
+        os.system(f"sh {data_dict[dataset_id]}")
