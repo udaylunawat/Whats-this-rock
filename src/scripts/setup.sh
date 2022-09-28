f5c81e968df6b17d11e784c88af924db9227d7c4
udaylunawat <udaylunawat@gmail.com>
2022-09-28 23:18:49 +0530

    quiet

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 # installing
 apt install -qq --allow-change-held-packages libcudnn8=8.1.0.77-1+cuda11.2
-pip install -r requirements-dev.txt
+pip install -r requirements-dev.txt --quiet
 
 # setting up kaggle
 wget -qq https://www.dropbox.com/s/ltp4ly8ilvxlgas/kaggle.json
