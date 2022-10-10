bb3e3110698383b30643de2554bd6857c20874bf
udaylunawat <udaylunawat@gmail.com>
2022-10-10 12:50:18 +0530

    chore: upgrade cudnn+cuda version

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # installing
-apt install -qq --allow-change-held-packages libcudnn8=8.1.0.77-1+cuda11.2
+apt install -qq --allow-change-held-packages libcudnn8=8.1.1.33-1+cuda11.2
 pip install -r requirements-dev.txt
 
 # setting up kaggle
