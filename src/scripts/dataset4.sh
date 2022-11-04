5326df4ebd93d7fc66f2da6ec27f7b2fe3d9ff23
udaylunawat <udaylunawat@gmail.com>
2022-11-04 21:17:06 +0530

    fix: incorrect path

--- src/scripts/dataset4.sh
+++ src/scripts/dataset4.sh
@@ -4,5 +4,5 @@
 kaggle datasets download neelgajare/rocks-dataset --path data/0_raw/
 unzip -qn data/0_raw/rocks-dataset.zip -d data/1_extracted/
 mkdir -p data/1_extracted/dataset4/rock_classes/
-mv -n data/1_extracted/Rocks/* data/1_extracted/dataset4/
+mv -n data/1_extracted/Rocks/* data/1_extracted/dataset4/rock_classes
 rm -rf data/1_extracted/Rocks
