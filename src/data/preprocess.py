51ca4de6c3e910d11b8d4d675e296cd20108264e
udaylunawat <udaylunawat@gmail.com>
2022-11-05 18:32:15 +0530

    fix: hydra

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 import logging
-
+import hydra
 
 from src.data.utils import (
     get_df,
@@ -16,6 +16,7 @@ from src.data.utils import (
 )
 
 
+@hydra.main(config_path="../../configs", config_name="config", version_base='1.2')
 def process_data(cfg):
     """Download dataset, removes unsupported and corrupted images, and splits data into train, val and test.
 
@@ -36,9 +37,9 @@ def process_data(cfg):
     print("\nFile types before cleaning:")
     get_value_counts("data/2_processed")
 
-    move_bad_files("configs/bad_images selected by gemini.txt", "data/bad_images")
-    move_bad_files("configs/misclassified selected by gemini.txt", "data/misclassified_images")
-    move_bad_files("configs/duplicates selected by gemini.txt", "data/duplicate_images")
+    move_bad_files("configs/bad_images selected by gemini.txt", "data/bad_images", "Moving bad images...")
+    move_bad_files("configs/misclassified selected by gemini.txt", "data/misclassified_images", "Moving misclassified images...")
+    move_bad_files("configs/duplicates selected by gemini.txt", "data/duplicate_images", "Moving duplicate images...")
 
     remove_unsupported_images("data/2_processed")
     remove_corrupted_images("data/2_processed")
