eab10470266f33de5fb188586d54fc867a677563
udaylunawat <udaylunawat@gmail.com>
2022-11-05 15:55:06 +0530

    ref

--- src/scripts/clean_dir.sh
+++ src/scripts/clean_dir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setting up data dir
-rm -rf data/2_processed data/3_tfds_dataset data/corrupted_images data/bad_images
+rm -rf data/1_extracted data/2_processed data/3_tfds_dataset data/corrupted_images data/bad_images
 mkdir -p data/0_raw data/1_extracted data/2_processed data/3_tfds_dataset data/corrupted_images data/bad_images checkpoints
 mkdir -p data/2_processed/Coal/
 mkdir -p data/2_processed/Basalt/
--- src/scripts/dataset1.sh
+++ src/scripts/dataset1.sh
@@ -4,3 +4,11 @@
 kaggle datasets download salmaneunus/rock-classification --path data/0_raw/
 unzip -qn data/0_raw/rock-classification.zip -d data/1_extracted/
 mv -vn data/1_extracted/Dataset data/1_extracted/dataset1
+
+mv data/1_extracted/dataset1/Igneous/* data/1_extracted/dataset1/
+mv data/1_extracted/dataset1/Metamorphic/* data/1_extracted/dataset1/
+mv data/1_extracted/dataset1/Sedimentary/* data/1_extracted/dataset1/
+
+rm -rf data/1_extracted/dataset1/Igneous/
+rm -rf data/1_extracted/dataset1/Metamorphic/
+rm -rf data/1_extracted/dataset1/Sedimentary/
