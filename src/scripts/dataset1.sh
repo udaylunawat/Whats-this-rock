283079bf58cb20bfffec9a70d9e6f9ef3a04fb10
udaylunawat <udaylunawat@gmail.com>
2022-11-07 15:01:05 +0530

    removed kaggle

--- src/scripts/dataset1.sh
+++ src/scripts/dataset1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # dataset 1 processing
-kaggle datasets download salmaneunus/rock-classification --path data/0_raw/
+wget --quiet -O data/0_raw/rock-classification.zip https://huggingface.co/datasets/udayl/rocks/resolve/main/rock-classification.zip
 unzip -qn data/0_raw/rock-classification.zip -d data/1_extracted/
 mv -vn data/1_extracted/Dataset data/1_extracted/dataset1
 
--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -4,12 +4,6 @@
 apt install -qq --allow-change-held-packages libcudnn8=8.1.1.33-1+cuda11.2
 pip install -r requirements-dev.txt
 
-# setting up kaggle
-wget -qq https://www.dropbox.com/s/ltp4ly8ilvxlgas/kaggle.json
-mkdir -p ~/.kaggle
-mv -n kaggle.json ~/.kaggle/
-chmod 600 ~/.kaggle/kaggle.json
-
 sh src/scripts/clean_dir.sh
 python src/data/download.py
 python src/data/preprocess.py
