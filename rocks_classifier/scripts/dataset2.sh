2518d92fcd2c14407911d6e0fddb860dec488c95
udaylunawat <udaylunawat@gmail.com>
2022-11-08 13:06:46 +0530

    ref: kaggle -> wget_huggingface datasets

--- rocks_classifier/scripts/dataset2.sh
+++ rocks_classifier/scripts/dataset2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # dataset 2 processing
-kaggle datasets download mahmoudalforawi/igneous-metamorphic-sedimentary-rocks-and-minerals --path data/0_raw/
+wget --quiet -O data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip https://huggingface.co/datasets/udayl/rocks/resolve/main/igneous-metamorphic-sedimentary-rocks-and-minerals.zip
 unzip -qn data/0_raw/igneous-metamorphic-sedimentary-rocks-and-minerals.zip -d data/1_extracted/
 mv data/1_extracted/Rock_Dataset data/1_extracted/dataset2
 
