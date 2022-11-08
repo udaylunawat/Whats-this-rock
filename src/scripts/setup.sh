398943906c51ca285310927d33cccced84bbedbf
udaylunawat <udaylunawat@gmail.com>
2022-11-08 13:03:26 +0530

    fix: src -> rocks_classifier

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -4,6 +4,6 @@
 apt install -qq --allow-change-held-packages libcudnn8=8.1.1.33-1+cuda11.2
 pip install -r requirements-dev.txt
 
-sh src/scripts/clean_dir.sh
-python src/data/download.py
-python src/data/preprocess.py
+sh rock_classifier/scripts/clean_dir.sh
+python rock_classifier/data/download.py
+python rock_classifier/data/preprocess.py
