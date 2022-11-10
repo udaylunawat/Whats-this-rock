e76dc99b4c0e217200c31fe01070d29c4c9f216b
udaylunawat <udaylunawat@gmail.com>
2022-11-10 23:23:49 +0530

    fix:: added all config files

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     url = cfg['git_url'],
     packages = setuptools.find_packages(),
     include_package_data = True,
-    package_data={'configs': ['*.txt', '*.yaml']},
+    package_data={'': ['*.txt', '*.yaml', '*.py',]},
     install_requires = requirements,
     extras_require={'dev': dev_requirements,
                     'gpu': ['tensorflow-gpu'],
